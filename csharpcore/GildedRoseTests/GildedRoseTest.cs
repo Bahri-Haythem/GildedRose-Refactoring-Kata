2e0fdab5ab306880a562565958926f254e7eec26
Haythem Bahri <haythem.bahri8@hotmail.com>
2024-01-17 10:05:10 +0100

    first try

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,38 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
-    public void Foo()
+    public void quality_is_less_than_50()
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 50 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.AreEqual("fixme", items[0].Name);
+        Assert.LessOrEqual(items[0].Quality, 50);
+    }
+
+    [Test]
+    public void quality_is_positive()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 50 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.Greater(items[0].Quality, 0);
+    }
+
+    [Test]
+    public void quality_Sulfuras_is_less_than_80()
+    {
+        var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.LessOrEqual(items[0].Quality, 80);
+    }
+
+    [Test]
+    public void aged_brie_test()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -24, Quality = 50 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.AreEqual(items[0].Quality, 50);
     }
 }
